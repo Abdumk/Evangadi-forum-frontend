32bd0bcc4d27043c8907344d9cce5f7dd7f2bc19
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2024-12-20 21:05:12 +0300

    browserRouter wrap fixed

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,8 +3,10 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
+const baseURL = "/Evangadi-forum-frontend";
+
 createRoot(document.getElementById("root")).render(
-    <Router>
+    <Router basename={baseURL}>
         <App />
     </Router>
 );
--- Client/vite.config.js
+++ Client/vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Replace <REPO_NAME> with your GitHub repository name
+  base: '/Evangadi-forum-frontend/', // Replace <REPO_NAME> with your GitHub repository name
 });
