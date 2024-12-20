f572bafd0bba516ca311a7dd89e56830cac5d9c7
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2024-12-20 16:26:38 +0300

    deploy

--- Client/src/utility/axios.js
+++ Client/src/utility/axios.js
@@ -9,5 +9,7 @@ export const axiosInstance = axios.create({
   // baseURL: `http://localhost:${Ports}/api`,
 
   // deployed endpoint reference
-  baseURL: "http://localhost:3004/api/",
+  // baseURL: "http://localhost:3004/api/",
+  // Deployed version of EVANGADI-FOURM-BACK END
+  baseURL: "https://evangadi-forum-back-end-abd.onrender.com/api"
 });
